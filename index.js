c916529d918a9ec4beb8f7c96f75ddc786091a23
Felipe <126893383+feoliveira7@users.noreply.github.com>
2024-08-28 00:19:44 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+function scrollToSobre() {
+    // Obtém a div alvo
+    var targetDiv = document.getElementById("divSobre");
+    
+    // Usa scrollIntoView para rolar até a div
+    targetDiv.scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToTime() {
+    // Obtém a div alvo
+    var targetDiv = document.getElementById("divTime");
+    
+    // Usa scrollIntoView para rolar até a div
+    targetDiv.scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToBaixar() {
+    // Obtém a div alvo
+    var targetDiv = document.getElementById("divBaixar");
+    
+    // Usa scrollIntoView para rolar até a div
+    targetDiv.scrollIntoView({ behavior: "smooth" });
+}
+
+
+
